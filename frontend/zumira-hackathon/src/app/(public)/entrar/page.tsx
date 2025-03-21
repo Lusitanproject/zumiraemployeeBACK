7b83624e3662e29f7b454d5ce0523428cc767ac5
Gabriel Brevilieri <gbrevilieri@gmail.com>
2025-03-21 16:43:58 -0300

    feat: added loading bar and image to mobile login screen

--- frontend/zumira-hackathon/src/app/(public)/entrar/page.tsx
+++ frontend/zumira-hackathon/src/app/(public)/entrar/page.tsx
@@ -2,19 +2,26 @@ import Image from "next/image";
 import logo from "../../../../public/logo-zumira.png";
 import { SignUpForm } from "./form";
 import { borel } from "@/app/fonts";
+import { HeadImage } from "@/components/custom/head-image";
 
 export default function Entrar() {
   return (
     <div className="w-full md:w-96 mx-auto flex flex-col items-center">
-      <Image src={logo} width={127} height={40} alt="Logo Zumira" className="mb-12 md:mb-14" />
+      <Image src={logo} width={127} height={40} alt="Logo Zumira" className="mb-12 md:mb-14" unoptimized />
+
       {/* IMAGEM CASO SEJA EM TELAS MENORES */}
+      <div className="md:hidden flex">
+        <HeadImage size="medium" />
+      </div>
+
       <div className="mb-8">
         <p className="text-[1.625rem] md:text-3xl font-semibold text-center text-gray-700 tracking-tight">
-          Entre e comece a desfrutar<br />
+          Entre e comece a desfrutar
+          <br />
           de todos os <span className={`${borel.className} text-primary-500 tracking-normal`}>benefícios</span>
         </p>
       </div>
       <SignUpForm />
     </div>
-  )
+  );
 }
--- frontend/zumira-hackathon/src/app/(public)/layout.tsx
+++ frontend/zumira-hackathon/src/app/(public)/layout.tsx
@@ -1,19 +1,17 @@
-import { HeadImage } from "@/components/custom/head-image"
-import { ReactNode } from "react"
+import { HeadImage } from "@/components/custom/head-image";
+import { ReactNode } from "react";
 
 type LayoutProps = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
 export default function PublicLayout({ children }: LayoutProps) {
   return (
     <div className="w-full h-screen overflow-hidden grid grid-cols-1 md:grid-cols-[58vw_42vw] grid-rows-1">
       <section className="bg-primary-25 flex items-center justify-center">
-        <HeadImage />
+        <HeadImage size="large" fadeColor="var(--color-primary-25)" />
       </section>
-      <div className="h-screen px-4 py-14 bg-white overflow-y-auto">
-        {children}
-      </div>
+      <div className="h-screen px-4 py-14 bg-white overflow-y-auto">{children}</div>
     </div>
-  )
+  );
 }
