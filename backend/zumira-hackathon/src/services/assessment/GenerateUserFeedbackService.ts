71fb906bf4a6118a8b9b14a311db7810dd668889
Gabriel Brevilieri <gbrevilieri@gmail.com>
2025-04-25 13:00:24 -0300

    chore: small optimization

--- backend/zumira-hackathon/src/services/assessment/GenerateUserFeedbackService.ts
+++ backend/zumira-hackathon/src/services/assessment/GenerateUserFeedbackService.ts
@@ -54,81 +54,57 @@ async function sendMessage(instructions: string | null, message: string) {
 
 class GenerateUserFeedbackService {
   async execute({ userId, assessmentId }: GenerateUserFeedbackRequest) {
-    const latestResult = await prismaClient.assessmentResult.findFirst({
+    const result = await prismaClient.assessmentResult.findFirst({
       where: {
         assessmentId,
         userId,
       },
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
-    if (!latestResult) throw new Error("No results for this assessment");
-
-    const assessment = await prismaClient.assessment.findFirst({
-      where: {
-        id: assessmentId,
-      },
-      select: {
-        userFeedbackInstructions: true,
-        operationType: true,
-        assessmentQuestions: {
-          select: {
-            psychologicalDimension: {
-              select: {
-                id: true,
-                name: true,
-              },
-            },
-            assessmentQuestionAnswers: {
-              where: {
-                userId: userId,
-                assessmentResultId: latestResult.id,
-              },
+      include: {
+        assessment: true,
+        assessmentQuestionAnswers: {
+          include: {
+            assessmentQuestionChoice: true,
+            assessmentQuestion: {
               include: {
-                assessmentQuestionChoice: true,
+                psychologicalDimension: true,
               },
             },
           },
         },
       },
+      orderBy: {
+        createdAt: "desc",
+      },
     });
-    if (!assessment) throw new Error("Assessment does not exist");
+    if (!result) throw new Error("No results for this assessment");
 
-    const dimensions = [] as { data: { id: string; name: string }; values: number[] }[];
-    assessment.assessmentQuestions.map((q) => {
-      if (!dimensions.find((d) => d.data.id === q.psychologicalDimension.id)) {
-        dimensions.push({ data: q.psychologicalDimension, values: [] });
-      }
+    const dimensionAnswersValues: Record<string, number[]> = {};
+    result.assessmentQuestionAnswers.map((answer) => {
+      const dimension = answer.assessmentQuestion.psychologicalDimension.name;
+
+      if (!dimensionAnswersValues[dimension]) dimensionAnswersValues[dimension] = [];
+      dimensionAnswersValues[dimension].push(answer.assessmentQuestionChoice.value);
     });
 
-    for (const question of assessment.assessmentQuestions) {
-      const answer = question.assessmentQuestionAnswers[0];
-      if (!answer) continue;
+    const message = Object.entries(dimensionAnswersValues)
+      .map(([dimension, values]) => {
+        const sum = values.reduce((sum, v) => sum + v, 0);
+        const average = sum / values.length;
 
-      const dimension = dimensions.find((d) => d.data.id === question.psychologicalDimension.id);
-      dimension?.values.push(answer.assessmentQuestionChoice.value);
-    }
-
-    const message = dimensions
-      .filter((d) => d.values.length)
-      .map((d) => {
-        const sum = d.values.reduce((sum, v) => sum + v, 0);
-        const average = sum / d.values.length;
-
-        switch (assessment.operationType) {
+        switch (result.assessment.operationType) {
           case "SUM":
-            return `${d.data.name}: ${sum.toFixed(2)}`;
+            return `${dimension}: ${sum.toFixed(2)}`;
           case "AVERAGE":
-            return `${d.data.name}: ${average.toFixed(2)}`;
+            return `${dimension}: ${average.toFixed(2)}`;
         }
       })
       .join(", ");
+
     if (!message) throw new Error("No values to send");
 
     console.log(`Generating user feedback for assessment ${assessmentId}`);
 
-    const response = await sendMessage(assessment.userFeedbackInstructions, message);
+    const response = await sendMessage(result.assessment.userFeedbackInstructions, message);
 
     const assessmentFeeedback = await prismaClient.userAssessmentFeedback.create({
       data: {
