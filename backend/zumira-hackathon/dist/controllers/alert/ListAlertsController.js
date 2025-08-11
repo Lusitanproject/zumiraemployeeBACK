bc0b9846371ea5d3badd29cd08701848ad43a499
Gabriel Brevilieri <gbrevilieri@gmail.com>
2025-08-11 10:18:14 -0300

    chore: build

--- backend/zumira-hackathon/dist/controllers/alert/ListAlertsController.js
+++ backend/zumira-hackathon/dist/controllers/alert/ListAlertsController.js
@@ -2,10 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ListAlertsController = void 0;
 const ListAlertsService_1 = require("../../services/alert/ListAlertsService");
+const alert_1 = require("../../definitions/alert");
+const parseZodError_1 = require("../../utils/parseZodError");
 class ListAlertsController {
     async handle(req, res) {
+        const { error, data, success } = alert_1.ListAlertsSchema.safeParse(req.query);
+        if (!success)
+            throw new Error((0, parseZodError_1.parseZodError)(error));
         const service = new ListAlertsService_1.ListAlertsService();
-        const result = await service.execute(req.user.id);
+        const result = await service.execute({ userId: req.user.id, ...data });
         return res.json({ status: "SUCCESS", data: result });
     }
 }
