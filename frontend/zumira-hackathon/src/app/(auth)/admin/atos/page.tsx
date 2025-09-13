c3ef3d83097809a3b6c5958103d75246ecf3b020
Gabriel Brevilieri <gbrevilieri@gmail.com>
2025-09-13 15:04:53 -0300

    fix

--- frontend/zumira-hackathon/src/app/(auth)/admin/atos/page.tsx
+++ frontend/zumira-hackathon/src/app/(auth)/admin/atos/page.tsx
@@ -2,9 +2,9 @@ import { getTrailsAdmin } from "@/api/trails";
 
 import { ActsList } from "./components/list";
 
-export default async function Atos({ searchParams }: { searchParams: { trailId?: string } }) {
+export default async function Atos({ searchParams }: { searchParams: Promise<{ trailId?: string }> }) {
   const trails = await getTrailsAdmin();
-  const trailId = searchParams.trailId ?? trails[0]?.id;
+  const trailId = (await searchParams).trailId ?? trails[0]?.id;
 
   return (
     <div className="flex flex-col w-full">
