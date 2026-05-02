23d78892ba4e60a745f97393cd94cc713578f749
Gabriel Brevilieri <gbrevilieri@gmail.com>
2026-05-02 19:20:41 -0300

    chore: remove unused routers

--- src/_routes/nationalities.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { ListNationalitiesController } from "../controllers/nationality/ListNationalitiesController";
-
-const nationalitiesRoutes = Router();
-
-nationalitiesRoutes.get("/", new ListNationalitiesController().handle);
-
-export { nationalitiesRoutes };
--- src/_routes/users.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { CreateUserController } from "../controllers/user/CreateUserController";
-
-const usersRoutes = Router();
-
-usersRoutes.post("/", new CreateUserController().handle);
-
-export { usersRoutes };
