a0a736c518c44d41134ee74061c49267b3d66696
Gabriel Brevilieri <gbrevilieri@gmail.com>
2026-05-06 10:21:50 -0300

    chore: remove build dir

--- dist/routes/nationalities.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.nationalitiesRoutes = void 0;
-const express_1 = require("express");
-const ListNationalitiesController_1 = require("../controllers/nationality/ListNationalitiesController");
-const nationalitiesRoutes = (0, express_1.Router)();
-exports.nationalitiesRoutes = nationalitiesRoutes;
-nationalitiesRoutes.get("/", new ListNationalitiesController_1.ListNationalitiesController().handle);
